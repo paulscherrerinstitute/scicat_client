82dc9f99d8452ebd21be6176fd5624b79c366f4a
Sala Leonardo <leonardo.sala@psi.ch>
2019-03-08 15:06:59 +0100

    added example scripts to check unsaved data in directories

---
+++ check_saved_data.py
@@ -0,0 +1,72 @@
+import scicat_client
+import os
+from pprint import pprint
+
+
+if __name__ == "__main__":
+
+    import argparse
+    from pprint import pprint
+    import sys
+
+    parser = argparse.ArgumentParser(usage="This is a test")
+    parser.add_argument("pgroup", type=str, help="boh")
+    parser.add_argument("datadir", type=str, help="boh")
+    parser.add_argument("filelist", type=str, help="boh")
+
+    args = parser.parse_args()
+
+    filelist = args.filelist
+    pgroup = args.pgroup
+    datadir = args.datadir
+
+    includes = [".h5"]
+    
+    # raw
+    scicat_files = {}
+    with open(filelist) as f:
+        for line in f:
+            scicat_files[line.strip("\n")] = 1
+
+    unsaved_files = []
+    
+    for dirpath, dirnames, filenames in os.walk(os.path.join(datadir, "raw")):
+        for f in filenames:
+            full_f = os.path.join(dirpath, f)
+            proceed = False
+            
+            for include in includes:
+                if full_f.find(include) != -1:
+                    proceed = True
+                    continue
+            if not proceed:
+                continue
+            
+            if full_f in scicat_files:
+                _ = scicat_files.pop(full_f)
+            else:
+                unsaved_files.append(full_f)
+                
+    for dirpath, dirnames, filenames in os.walk(os.path.join(datadir, "res")):
+        for f in filenames:
+            full_f = os.path.join(dirpath, f)
+            proceed = False
+            for include in includes:
+                if full_f.find(include) != -1:
+                    proceed = True
+                    continue
+            if not proceed:
+                continue
+            if full_f in scicat_files:
+                _ = scicat_files.pop(full_f)
+            else:
+                unsaved_files.append(full_f)
+
+        
+    for f in unsaved_files:
+        print(f)
+    
+    
+    
+    
+    
---
+++ check_saved_datasets.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+pgroups="p16581 p16582 p16583 p17245 p17296 p17295 p17536"
+
+echo "PGROUP SAVED NOTSAVED"
+for pgroup in $pgroups; do
+    #python scicat_client.py dump_filelist -g $pgroup > filelist_${pgroup}.txt
+    #python check_saved_data.py $pgroup /sf/alvra/data/$pgroup filelist_$pgroup.txt > \
+    #       filelist_notsavedh5_$pgroup.txt 
+    #python check_saved_data.py $pgroup /sf/bernina/data/$pgroup filelist_$pgroup.txt >> \
+    #       filelist_notsavedh5_$pgroup.txt
+
+    saved=$(wc -l filelist_$pgroup.txt | awk '{print $1}')
+    notsaved=$(wc -l filelist_notsavedh5_$pgroup.txt | awk '{print $1}')
+    echo $pgroup $saved $notsaved
+done
+
