34a326a8f9330ab7faa69f4bdc3992f2d9f6b14f
Leonardo Sala <leonardo.sala@psi.ch>
2021-11-05 15:24:58 +0100

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name="scicat_client",
-    version="0.1.0",
+    version="0.2.1",
     author="Paul Scherrer Institute",
     author_email="science-it@psi.ch",
     description=("Interface to PSI's SciCat"),
